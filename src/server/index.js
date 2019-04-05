b911b84eb3fb978f4e33cb13e0ee03885f5e4446
phatnth-ibl <phattnh@blockchainlabs.asia>
2019-04-05 14:48:53 +0700

    update package.json

--- src/server/index.js
+++ src/server/index.js
@@ -27,7 +27,8 @@ app.get('*', (req, res, next) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Vexere</title>
+        <link rel="icon" href="assets/img/marvel-icon.ico">    
+        <title>Marvel App</title>
     </head>
     <body>
         <div id="root"></div>
