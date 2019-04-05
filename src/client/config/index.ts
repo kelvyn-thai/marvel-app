23a53f2fe1d06201f965ced9a432b389dcc8ca07
phatnth-ibl <phattnh@blockchainlabs.asia>
2019-04-05 15:02:20 +0700

    fix https

--- src/client/config/index.ts
+++ src/client/config/index.ts
@@ -2,5 +2,5 @@ export const marvelApiConfigs = {
     ts: 1,
     apikey: '22c3fa0ee15cee85210e42f6bac742f1',
     hash: '4f021a1c41639f51b0a0051806f40e9a',
-    domain: 'http://gateway.marvel.com/v1/public'
+    domain: 'https://gateway.marvel.com/v1/public'
 }
